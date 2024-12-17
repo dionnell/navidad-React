85f7f17d2b6205173c883c9baf37f6676f216045
dionnell <dionnellr1@gmail.com>
2024-12-17 19:13:37 -0300

    reconocimiento de voz

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import 'regenerator-runtime/runtime'
 
 import './index.css'
 import { NavidadApp } from './NavidadApp'
